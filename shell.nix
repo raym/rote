16be58d7b6f1b454aa3328c11cf291c36afa6f39
raym <raym@users.noreply.github.com>
2024-04-13 12:39:35 -0700

    add lunarvim to nix shell and remove __pycache__

--- shell.nix
+++ shell.nix
@@ -1,12 +1,13 @@
 let
   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-23.11";
-  pkgs = import nixpkgs { config = {}; overlays = []; };
+  pkgs = import nixpkgs { config = { }; overlays = [ ]; };
 in
 
 pkgs.mkShell {
   packages = with pkgs; [
     git
     neovim
+    lunarvim
     python3
   ];
 
