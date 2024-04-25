7be991840f68493340377a43cf7e203115d470ac
raym <raym@users.noreply.github.com>
2024-04-25 15:12:36 -0700

    visually print return character so it can be colored red if it's wrong

--- user_interactions/test_user.py
+++ user_interactions/test_user.py
@@ -6,10 +6,18 @@ def render_screen(stdscr, question, answer, guess):
     stdscr.clear()
     stdscr.addstr(question)
     start_answer_on_line = calculate_linecount(question) + 1
+    line_num = 0
+    col_num = 0
     for i, char in enumerate(guess):
         correct_char = answer[i] if i < len(answer) else None
         color = curses.color_pair(SUCCESS_TEXT) if char == correct_char else curses.color_pair(ERROR_TEXT)
-        stdscr.addstr(start_answer_on_line, i, char, color)
+        char_to_print = '↵\n' if char == '\n' else char
+        stdscr.addstr(start_answer_on_line + line_num, col_num, char_to_print, color)
+        if char == '\n':
+            line_num += 1
+            col_num = 0
+        else:
+            col_num += 1
     stdscr.refresh()
 
 def test_user(stdscr, card):
